c78e016d0acc22f4a00bba0b0fb39df1ba32d969
veesus mikel heir <noneyourbusiness@nothing.com>
2018-10-28 14:21:18 -0400

    Started adding very very basic terrain generation

--- src/main/java/abnormals/frist/common/world/ChunkGeneratorFrist.java
+++ src/main/java/abnormals/frist/common/world/ChunkGeneratorFrist.java
@@ -1,10 +1,12 @@
 package abnormals.frist.common.world;
 
+import abnormals.frist.common.init.ModBlocks;
 import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.ChunkPrimer;
 import net.minecraft.world.gen.IChunkGenerator;
 
 import javax.annotation.Nullable;
@@ -22,13 +24,32 @@ public class ChunkGeneratorFrist implements IChunkGenerator {
     public Chunk generateChunk(int x, int z) {
         int globalCoordX = x<<4;
         int globalCoordZ = z<<4;
-        Chunk chunk =  new Chunk(world,x,z);
+        ChunkPrimer primer = new ChunkPrimer();
+
+
+
+
         this.biomeBufferArray =world.getBiomeProvider().getBiomes(this.biomeBufferArray,globalCoordX,globalCoordZ,16,16);
+
+        generateBaseTerrainBlocks(primer);
+
+        Chunk chunk =  new Chunk(world,primer,x,z);
+
         byte[] chunkBiomeByteArray = chunk.getBiomeArray();
         for(int i = 0; i<chunkBiomeByteArray.length;i++){
             chunkBiomeByteArray[i] =(byte)Biome.getIdForBiome(biomeBufferArray[i]);
         }
         return chunk;
+    }
+
+    public void generateBaseTerrainBlocks(ChunkPrimer primer){
+        for(int x = 0; x<16;x++){
+            for(int z = 0; z<16;z++){
+                for(int y = 0; y<64; y++){
+                    primer.setBlockState(x,y,z, ModBlocks.BLUE_FRISTUM.getDefaultState());
+                }
+            }
+        }
     }
 
     @Override
