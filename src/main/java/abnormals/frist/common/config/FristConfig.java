6532594b7d3d47592f85d4d5afd4686599c4d508
veesus mikel heir <noneyourbusiness@nothing.com>
2018-10-28 06:53:12 -0400

    Started adding a basic FristConfig

---
+++ src/main/java/abnormals/frist/common/config/FristConfig.java
@@ -0,0 +1,8 @@
+package abnormals.frist.common.config;
+
+import abnormals.frist.Frist;
+import net.minecraftforge.common.config.Config;
+
+@Config(modid = Frist.MODID)
+public class FristConfig {
+}
